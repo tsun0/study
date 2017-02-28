97cc43bf6be9e4872bf077e55c1e5f69d0a0853c
Erika Tsuchiya <tsuchieri_1111@yahoo.co.jp>
2017-02-28 10:54:37 +0900

    テーブル削除のファイルを追加

---
+++ DynamoDB/deleteTable.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+func main() {
+	// セッションを持つDynamoDBクライアントの新しいインスタンスを作成
+	ddb := dynamodb.New(session.New(), aws.NewConfig().WithRegion("us-east-1"))
+
+	// DeleteTable操作の入力を表します。
+	params := &dynamodb.DeleteTableInput{
+		TableName: aws.String("TableName"), // 削除するテーブルの名前
+	}
+
+	// DeleteTableは、テーブルとそのすべてのアイテムを削除します。
+	resp, err := ddb.DeleteTable(params)
+
+	if err != nil {
+		fmt.Println(err.Error()) // エラー処理
+	}
+
+	fmt.Println(resp)
+}
